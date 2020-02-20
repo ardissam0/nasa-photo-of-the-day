96930503eec2862f7b12b04d03575facaf4c419b
Sam Ardis <ardis.productions@gmail.com>
2020-02-20 13:33:51 -0500

    Added day two mvp I think! tweaked styling using styled-components, etc

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import './Loading.css';
+// import './Loading.css';
+
+import styled from 'styled-components';
+
+const LoadingDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    position: relative; 
+    `;
 
 function Loading() {
     return (
-        <div className= 'loading'>
+            <LoadingDiv>
             <img
             src ='https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif'
             alt='Loading'></img>
-        </div>
+            </LoadingDiv>
     );
 }
 
--- src/components/PicOfTheDay.js
+++ src/components/PicOfTheDay.js
@@ -1,19 +1,66 @@
 import React from 'react';
-import './PicOfTheDay.css';
+// import './PicOfTheDay.css';
+
+import styled from 'styled-components';
+
+const ImgDiv = styled.div`
+display: block;
+    width: 55%;
+    height: 100%;
+    margin: 0 auto;
+    padding: 5% 5%;
+    border: 10px solid black;
+
+    font-family: 'Open Sans', sans-serif;
+    background-color: coral;
+    color: black;
+    display: flex;
+    flex-direction: column;
+
+    -webkit-box-shadow: 5px 5px 15px 5px white;
+    box-shadow: 5px 5px 15px white;
+    `;
+
+    const Img = styled.img`
+    width: 100%
+    `;
+
+    const TitleDiv = styled.div`
+    font-size: 1.6rem;
+    font-weight: 800;
+    text-align: start;
+    line-height: 5.5rem;
+    `;
+    
+    const H2Div = styled.div`
+    margin: 0;
+    text-align: start;
+    `;
+
+    const PDiv = styled.div`
+    text-align: start;
+    width: 100%;
+    `;
+
+    const TxtBottomDiv = styled.div`
+    text-align: end;
+    margin: 0rem 0 1rem;
+    `;
+
 
 const PicOfTheDay = ({date, title, url, explanation}) => {
     return (
-        <div className ='image'>
+        <ImgDiv>
             <div>
-                <img src ={url} alt ='Nasa' />
+                <Img src ={url} alt ='Nasa' />
             </div>
-            <div className='h2'>Date: {date}</div>
-            <span className='textBottom'>By: NASA</span>
-            <div className='title'>{title}</div>
+            <H2Div>Date: {date}</H2Div>
+            <TxtBottomDiv>By: NASA</TxtBottomDiv>
+            <TitleDiv>{title}</TitleDiv>
             <div>
-                <p className='exp'>{explanation}</p>
+                <PDiv>{explanation}</PDiv>
             </div>
-        </div>
+        </ImgDiv>
     );
 };
 
